de38abd7714f608e7640f46ffdf0c51d4c662404
Daryll Capistrano <dcapistrano.dev@gmail.com>
2021-03-19 20:13:43 -0700

    add cart functionality

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,13 +1,26 @@
 import React from "react";
 import { Container, Typography, Button, Grid } from "@material-ui/core";
+import CartItem from "./CartItem/CartItem";
 
 import useStyles from "./styles";
+import { Link } from "react-router-dom";
 
-const Cart = ({ cart }) => {
+const Cart = ({
+  cart,
+  handleUpdateCartQty,
+  handleRemoveFromCart,
+  handleEmptyCart,
+}) => {
   const classes = useStyles();
 
   const EmptyCart = () => (
-    <Typography variant="subtitle1">You have nothing in the cart</Typography>
+    <Typography variant="subtitle1">
+      You have nothing in the cart,
+      <Link to="/" className={classes.link}>
+        go back to add some
+      </Link>
+      !
+    </Typography>
   );
 
   const FilledCart = () => (
@@ -15,8 +28,11 @@ const Cart = ({ cart }) => {
       <Grid container spacing={3}>
         {cart.line_items.map((item) => (
           <Grid item xs={12} sm={4} key={item.id}>
-            {/* <CartItem /> */}
-            <div>{item.name}</div>
+            <CartItem
+              item={item}
+              onUpdateCartQty={handleUpdateCartQty}
+              onRemoveFromCart={handleRemoveFromCart}
+            />
           </Grid>
         ))}
       </Grid>
@@ -31,6 +47,7 @@ const Cart = ({ cart }) => {
             type="button"
             variant="contained"
             color="secondary"
+            onClick={handleEmptyCart}
           >
             Empty Cart
           </Button>
@@ -53,7 +70,9 @@ const Cart = ({ cart }) => {
   return (
     <Container>
       <div className={classes.toolbar} />
-      <Typography className={classes.title}>You Shopping Cart</Typography>
+      <Typography className={classes.title} gutterBottom>
+        You Shopping Cart
+      </Typography>
       {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   );
